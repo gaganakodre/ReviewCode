f4336f882a8d3e58aea08defe14f52e984829d4f
shreegowri <gaganandh@gmail.com>
2022-06-10 20:01:41 +0530

    [shreegowri] to calculate the simple interest

---
+++ SimpleIntesretReview/SimpleIntesretReview/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleIntesretReview
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            SimpleInterset simpleinterest = new SimpleInterset();
+            simpleinterest.Calculate();
+        }
+    }
+}
---
+++ SimpleIntesretReview/SimpleIntesretReview/SimpleInterset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIntesretReview
+{
+    internal class SimpleInterset
+    {
+        public void Calculate()
+        {
+            double SI = 0;
+            Console.WriteLine("enter the percentage value:");
+            double p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the Time period:");
+            double T = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the Rate value:");
+            double R = Convert.ToInt32(Console.ReadLine());
+            SI = p * T * R / 100;
+            Console.WriteLine("The simple interset:" + SI);
+
+
+
+        }
+    }
+}
